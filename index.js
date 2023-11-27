bf8d7b527c82db64bd9811fea727cc87bd3b41ee
JiHaD SeR <85656190+J-I-H-A-D@users.noreply.github.com>
2023-11-27 08:24:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const { name, author } = require(join(__dirname, "./package.json"));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('BOTWHAITA v5', {
+say('ZERAAH v2', {
     font: 'block', 
     align: 'center',
     gradient: ['white','white']})
